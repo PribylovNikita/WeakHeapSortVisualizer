be7d50bf8c240e0d8def6365aedd81ee51b21977
PribylovNikita <ghost115115@gmail.com>
2022-12-27 14:48:47 +0300

    removed irrelevant files

--- middle_state/stepState.java
+++
@@ -1,9 +0,0 @@
-package sample;
-
-public class stepState {
-    public WeakHeap.State state;
-    public Integer first;
-    public Integer second;
-    public boolean isChanged;
-    public int length;
-}
